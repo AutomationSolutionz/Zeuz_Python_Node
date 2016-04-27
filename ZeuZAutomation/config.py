0be9f92fd15ce9f1a8e042e736419839d4672298
Raju Ahmed Shetu <shetu2153@gmail.com>
2016-04-27 11:03:58 +0600

    deleted all for new restructure

--- ZeuZAutomation/config.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'shetu'
-from selenium import webdriver
-global WebDriver_Wait
-WebDriver_Wait = 20
-global WebDriver_Wait_Short
-WebDriver_Wait_Short = 10
-global sBrowser
-sBrowser=[]
