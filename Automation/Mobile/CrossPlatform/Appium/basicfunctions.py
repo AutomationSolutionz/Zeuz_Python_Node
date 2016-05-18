09317373bbd028cc6b1073c4e161471d8237ae19
Matiur Rahman Minar <minar09.BD@gmail.com>
2016-05-18 15:01:35 +0600

    appium basic functions!

--- Automation/Mobile/CrossPlatform/Appium/basicfunctions.py
+++ Automation/Mobile/CrossPlatform/Appium/basicfunctions.py
@@ -1,9 +1,15 @@
+
 # Android environment
 from appium import webdriver
 import os , sys, time, inspect
 from Utilities import CommonUtil
 from Automation.Mobile.Android.adb_calls import adbOptions
 from appium.webdriver.common.touch_action import TouchAction
+
+
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
 
 
 #if local_run is True, no logging will be recorded to the web server.  Only local print will be displayed
@@ -16,24 +22,40 @@ def launch(package_name,activity_name):
     try:
         sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
         CommonUtil.ExecLog(sModuleInfo,"Trying to launch the app",1,local_run)
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        df = adbOptions.get_android_version()
-        CommonUtil.ExecLog(sModuleInfo,df,1,local_run)
-        #adbOptions.kill_adb_server()
-        desired_caps['platformVersion'] = df
-        df = adbOptions.get_device_model()
-        CommonUtil.ExecLog(sModuleInfo,df,1,local_run)
-        #adbOptions.kill_adb_server()
-        desired_caps['deviceName'] = df
-        desired_caps['appPackage'] = package_name
-        desired_caps['appActivity'] = activity_name
-        driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-        global driver
-        CommonUtil.ExecLog(sModuleInfo,"Opened the app successfully",1,local_run)
-        time.sleep(10)
-        driver.implicitly_wait(5)
-        return "Passed"
+        
+        if 'driver' not in globals():
+            # appium driver not initiated.
+            try:
+                CommonUtil.ExecLog(sModuleInfo,"Now launching the app...",1,local_run)
+                desired_caps = {}
+                desired_caps['platformName'] = 'Android'
+                df = adbOptions.get_android_version()
+                CommonUtil.ExecLog(sModuleInfo,df,1,local_run)
+                #adbOptions.kill_adb_server()
+                desired_caps['platformVersion'] = df
+                df = adbOptions.get_device_model()
+                CommonUtil.ExecLog(sModuleInfo,df,1,local_run)
+                #adbOptions.kill_adb_server()
+                desired_caps['deviceName'] = df
+                desired_caps['appPackage'] = package_name
+                desired_caps['appActivity'] = activity_name
+                driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+                global driver
+                CommonUtil.ExecLog(sModuleInfo,"Launched the app successfully.",1,local_run)
+                time.sleep(10)
+                driver.implicitly_wait(5)
+                return "Passed"
+            except Exception, e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()        
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" +  "Error Message: " + str(exc_obj) +";" + "File Name: " + fname + ";" + "Line: "+ str(exc_tb.tb_lineno))
+                CommonUtil.ExecLog(sModuleInfo, "Unable to start WebDriver. %s"%Error_Detail, 3,local_run)
+                return "failed"
+        
+        else:
+            #driver already initiated.
+            open()
+    
     except Exception, e:
         exc_type, exc_obj, exc_tb = sys.exc_info()        
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -41,6 +63,8 @@ def launch(package_name,activity_name):
         CommonUtil.ExecLog(sModuleInfo, "Unable to start WebDriver. %s"%Error_Detail, 3,local_run)
         return "failed"
 
+
+        
 def close():
     sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
     try:
@@ -55,4 +79,95 @@ def close():
         Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" +  "Error Message: " + str(exc_obj) +";" + "File Name: " + fname + ";" + "Line: "+ str(exc_tb.tb_lineno))
         CommonUtil.ExecLog(sModuleInfo, "Unable to close the driver. %s"%Error_Detail, 3,local_run)
         return "failed"
+    
+    
+def install(app_location, app_package):
+    sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
+    try:
+        sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
+        CommonUtil.ExecLog(sModuleInfo,"Trying to install the app...",1,local_run)
+        
+        if 'driver' in globals():
+            #driver initiated
+            if driver.is_app_installed(app_package):
+                CommonUtil.ExecLog(sModuleInfo,"App is already installed.",1,local_run)
+            else:
+                CommonUtil.ExecLog(sModuleInfo,"App is not installed. Now installing...",1,local_run)
+                try:
+                    driver.install_app(app_location)
+                    CommonUtil.ExecLog(sModuleInfo,"App is installed successfully.",1,local_run)
+                    return "Passed"
+                except:
+                    CommonUtil.ExecLog(sModuleInfo, "Unable to install the app", 3,local_run)
+                    return "failed"
+        
+        else:
+            #driver not initiated
+            try:
+                CommonUtil.ExecLog(sModuleInfo,"Now installing and then launching the app...",1,local_run)
+                desired_caps = {}
+                desired_caps['platformName'] = 'Android'
+                df = adbOptions.get_android_version()
+                CommonUtil.ExecLog(sModuleInfo,df,1,local_run)
+                #adbOptions.kill_adb_server()
+                desired_caps['platformVersion'] = df
+                df = adbOptions.get_device_model()
+                CommonUtil.ExecLog(sModuleInfo,df,1,local_run)
+                #adbOptions.kill_adb_server()
+                desired_caps['deviceName'] = df
+                desired_caps['app'] = PATH(app_location)
+                driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+                global driver
+                CommonUtil.ExecLog(sModuleInfo,"Installed and launched the app successfully.",1,local_run)
+                time.sleep(10)
+                driver.implicitly_wait(5)
+                return "Passed"
+            except Exception, e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()        
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" +  "Error Message: " + str(exc_obj) +";" + "File Name: " + fname + ";" + "Line: "+ str(exc_tb.tb_lineno))
+                CommonUtil.ExecLog(sModuleInfo, "Unable to start WebDriver. %s"%Error_Detail, 3,local_run)
+                return "failed"
+
+    except Exception, e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()        
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" +  "Error Message: " + str(exc_obj) +";" + "File Name: " + fname + ";" + "Line: "+ str(exc_tb.tb_lineno))
+        CommonUtil.ExecLog(sModuleInfo, "Unable to start WebDriver. %s"%Error_Detail, 3,local_run)
+        return "failed"
+
+
+
+def open():
+    sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
+    try:
+        CommonUtil.ExecLog(sModuleInfo,"Trying to open the app",1,local_run)
+        driver.launch_app()
+        CommonUtil.ExecLog(sModuleInfo,"Opened the app successfully",1,local_run)
+        return "Passed"
+    except Exception, e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()        
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" +  "Error Message: " + str(exc_obj) +";" + "File Name: " + fname + ";" + "Line: "+ str(exc_tb.tb_lineno))
+        CommonUtil.ExecLog(sModuleInfo, "Unable to open the app. %s"%Error_Detail, 3,local_run)
+        return "failed"
+    
+
+def reset():
+    sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
+    try:
+        CommonUtil.ExecLog(sModuleInfo,"Trying to reset the app...",1,local_run)
+        driver.reset()
+        wait(5)
+        CommonUtil.ExecLog(sModuleInfo,"App is reset successfully",1,local_run)
+        return "Passed"
+    except Exception, e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()        
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" +  "Error Message: " + str(exc_obj) +";" + "File Name: " + fname + ";" + "Line: "+ str(exc_tb.tb_lineno))
+        CommonUtil.ExecLog(sModuleInfo, "Unable to reset the app. %s"%Error_Detail, 3,local_run)
+        return "failed"
+    
+    
+    
     
