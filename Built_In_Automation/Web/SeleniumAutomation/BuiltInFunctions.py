2396acf6496e06a72e2f6053797cc1232e61d04e
Riz <hossain.iqbal@gmail.com>
2016-05-20 00:19:39 -0400

    - first successful run...

--- Built_In_Automation/Web/SeleniumAutomation/BuiltInFunctions.py
+++ Built_In_Automation/Web/SeleniumAutomation/BuiltInFunctions.py
@@ -27,8 +27,8 @@ global WebDriver_Wait_Short
 WebDriver_Wait_Short = 10
 
 #if local_run is True, no logging will be recorded to the web server.  Only local print will be displayed
-local_run = True
-#local_run = False
+#local_run = True
+local_run = False
 
 def Open_Browser(browser):
     sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
