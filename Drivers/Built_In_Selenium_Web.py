d1486c871fecbdf42161fef65e4b7f6bb698b280
Matiur Rahman Minar <minar09.bd@gmail.com>
2016-05-21 20:10:40 +0600

    Built in selenium log fix!

--- Drivers/Built_In_Selenium_Web.py
+++ Drivers/Built_In_Selenium_Web.py
@@ -18,12 +18,13 @@ def open_browser(dependency,run_params,step_data,file_attachment,temp_q):
     #this function takes the dependency Browser and passes the browser type you selected during run time
     sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
     try:
-        
+        CommonUtil.ExecLog(sModuleInfo,"Enter: Step - Open Browser",1,local_run)
         #browser_name=dependency['dependency']['Browser']
         browser_name=dependency['Browser']
         stepReturn=BuiltInFunctions.Open_Browser(browser_name)
         CommonUtil.ExecLog(sModuleInfo,"started the browser",1,local_run)
         temp_q.put(stepReturn)
+        CommonUtil.ExecLog(sModuleInfo,"Exit: Step - Open Browser",1,local_run)
         return stepReturn
     except Exception, e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -55,11 +56,13 @@ def close_browser(dependency,run_time_params,step_data,file_attachment,temp_q):
 def go_to_webpage(dependency,run_params,step_data,file_attachment,temp_q):
     sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
     try:
+        CommonUtil.ExecLog(sModuleInfo,"Enter: Step - Go To Webpage",1,local_run)
         first_data_set=step_data[0]
         web_link=first_data_set[0][1]
         web_title = first_data_set[0][2]
         sTestStepReturnStatus=BuiltInFunctions.Go_To_Link(web_link, web_title)
         temp_q.put(sTestStepReturnStatus)
+        CommonUtil.ExecLog(sModuleInfo,"Exit: Step - Go To Webpage",1,local_run)
         return sTestStepReturnStatus
     except Exception, e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -72,17 +75,19 @@ def go_to_webpage(dependency,run_params,step_data,file_attachment,temp_q):
 def login_to_web_app(dependency,run_time_params,step_data,file_attachment,temp_q):
     sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
     try:
+        CommonUtil.ExecLog(sModuleInfo,"Enter: Step - Login To Web App",1,local_run)
         first_data_set=step_data[0]
         web_link=first_data_set[0][1]
         web_title = first_data_set[0][2]
         sTestStepReturnStatus=BuiltInFunctions.Go_To_Link(web_link, web_title)
         temp_q.put(sTestStepReturnStatus)
+        CommonUtil.ExecLog(sModuleInfo,"Exit: Step - Login To Web App",1,local_run)
         return sTestStepReturnStatus
     except Exception, e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" +  "Error Message: " + str(exc_obj) +";" + "File Name: " + fname + ";" + "Line: "+ str(exc_tb.tb_lineno))
-        CommonUtil.ExecLog(sModuleInfo, "Unable to go to webpage: Error:%s" %( Error_Detail), 3,local_run)
+        CommonUtil.ExecLog(sModuleInfo, "Unable to login to web app: Error:%s" %( Error_Detail), 3,local_run)
         temp_q.put("Failed")
         return "failed"
     
