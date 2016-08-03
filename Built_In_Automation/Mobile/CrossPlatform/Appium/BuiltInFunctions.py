5e6d784ee0dc25351bf4c2364fbb86a0a4e33f3d
Matiur Rahman Minar <minar09.bd@gmail.com>
2016-08-03 17:06:43 +0600

    started working on applium generic functions!

--- Built_In_Automation/Mobile/CrossPlatform/Appium/BuiltInFunctions.py
+++ Built_In_Automation/Mobile/CrossPlatform/Appium/BuiltInFunctions.py
@@ -452,3 +452,128 @@ def launch_ios_app():
         Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" +  "Error Message: " + str(exc_obj) +";" + "File Name: " + fname + ";" + "Line: "+ str(exc_tb.tb_lineno))
         CommonUtil.ExecLog(sModuleInfo, "Unable to start WebDriver. %s"%Error_Detail, 3,local_run)
         return "failed"
+
+
+
+#################################Generic functions###################################
+
+def Get_Element(element_parameter, element_value, reference_parameter=False, reference_value=False,
+                reference_is_parent_or_child=False, get_all_unvalidated_elements=False):
+    sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
+    try:
+        All_Elements_Found = []
+        if reference_is_parent_or_child == False:
+            if ((reference_parameter == False) and (reference_value == False)):
+                All_Elements = Get_All_Elements(element_parameter, element_value)
+                if ((All_Elements == []) or (All_Elements == 'failed')):
+                    CommonUtil.ExecLog(sModuleInfo, "Could not find your element by parameter:%s and value:%s..." % (
+                    element_parameter, element_value), 3, local_run)
+                    return "failed"
+                else:
+                    All_Elements_Found = All_Elements
+            elif (reference_parameter != False and reference_value != False):
+                CommonUtil.ExecLog(sModuleInfo, "Locating element using double matching", 1, local_run)
+                All_Elements = Get_Double_Matching_Elements(element_parameter, element_value, reference_parameter,
+                                                            reference_value)
+                if ((All_Elements == []) or (All_Elements == "failed")):
+                    CommonUtil.ExecLog(sModuleInfo,
+                                       "Could not find your element by parameter1:%s , value1:%s and parameter2:%s , value2:%s..." % (
+                                       element_parameter, element_value, reference_parameter, reference_value), 3,
+                                       local_run)
+                    return "failed"
+                else:
+                    All_Elements_Found = All_Elements
+            else:
+                CommonUtil.ExecLog(sModuleInfo,
+                                   "Could not find your element because you are missing at least one parameter", 3,
+                                   local_run)
+                return "failed"
+
+        elif reference_is_parent_or_child == "parent":
+            CommonUtil.ExecLog(sModuleInfo, "Locating all parents elements", 1, local_run)
+            all_parent_elements = Get_All_Elements(reference_parameter, reference_value)  # ,"parent")
+            all_matching_elements = []
+            for each_parent in all_parent_elements:
+                interested_elem = Get_All_Elements(element_parameter, element_value,
+                                                   each_parent)  # can there be a problem when we send in each parent, or does this contain both param and value?
+                if interested_elem != "failed":
+                    for each_matching in interested_elem:
+                        all_matching_elements.append(each_matching)
+            All_Elements_Found = all_matching_elements
+
+        elif reference_is_parent_or_child == "child":
+            all_parent_elements = Get_All_Elements(element_parameter, element_value)
+            all_matching_elements = []
+            for each_parent in all_parent_elements:
+                interested_elem = Get_All_Elements(reference_parameter, reference_value, each_parent)
+                if interested_elem != "failed":
+                    all_matching_elements.append(each_parent)
+            All_Elements_Found = all_matching_elements
+
+        elif ((reference_is_parent_or_child != "parent") or (reference_is_parent_or_child != "child") or (
+            reference_is_parent_or_child != False)):
+            CommonUtil.ExecLog(sModuleInfo,
+                               "Unspecified reference type; please indicate whether parent, child or leave blank", 3,
+                               local_run)
+            return "failed"
+
+        else:
+            CommonUtil.ExecLog(sModuleInfo,
+                               "Unable to run based on the current inputs, please check the inputs and re-enter values",
+                               3, local_run)
+            return "failed"
+
+        # this method returns all the elements found without validation
+        if (get_all_unvalidated_elements != False):
+            return All_Elements_Found
+        else:
+            # can later also pass on the index of the element we want
+            result = Element_Validation(All_Elements_Found)  # , index)
+            return result
+
+    except Exception, e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" + "Error Message: " + str(
+            exc_obj) + ";" + "File Name: " + fname + ";" + "Line: " + str(exc_tb.tb_lineno))
+        CommonUtil.ExecLog(sModuleInfo, "Could not find your element.  Error: %s" % (Error_Detail), 3, local_run)
+        return "failed"
+
+
+# Method to get the elements based on type - more methods may be added in the future
+# Called by: Get_Elements
+def Get_All_Elements(parameter, value, parent=False):
+    # http://selenium-python.readthedocs.io/locating-elements.html
+    sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
+    try:
+        if parent == False:
+            if parameter == "name":
+                All_Elements = driver.find_elements_by_name(value)
+        """else:
+            if parameter == "text":
+                All_Elements = WebDriverWait(parent, WebDriver_Wait).until(
+                    EC.presence_of_all_elements_located((By.XPATH, "//*[text()='%s']" % value)))
+            elif parameter == "tag":
+                All_Elements = WebDriverWait(parent, WebDriver_Wait).until(
+                    EC.presence_of_all_elements_located((By.TAG_NAME, '%s' % (value))))
+            elif ((parameter == "link_text") or (parameter == "href")):
+                All_Elements = WebDriverWait(parent, WebDriver_Wait).until(
+                    EC.presence_of_all_elements_located((By.LINK_TEXT, '%s' % (value))))
+            elif (parameter == "partial_link_text"):
+                All_Elements = WebDriverWait(parent, WebDriver_Wait).until(
+                    EC.presence_of_all_elements_located((By.PARTIAL_LINK_TEXT, '%s' % (value))))
+            elif parameter == "css":
+                All_Elements = WebDriverWait(parent, WebDriver_Wait).until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, '%s' % (value))))
+            else:
+                All_Elements = WebDriverWait(parent, WebDriver_Wait).until(
+                    EC.presence_of_all_elements_located((By.XPATH, "//*[@%s='%s']" % (parameter, value))))"""
+
+        return All_Elements
+    except Exception, e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" + "Error Message: " + str(
+            exc_obj) + ";" + "File Name: " + fname + ";" + "Line: " + str(exc_tb.tb_lineno))
+        CommonUtil.ExecLog(sModuleInfo, "Unable to get the element.  Error: %s" % (Error_Detail), 3, local_run)
+        return "failed"
