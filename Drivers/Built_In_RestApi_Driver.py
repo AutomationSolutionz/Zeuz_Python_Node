bbe6e9ee400ec0ded65f2ae39573b69ffbacc955
asifurrouf <rouf.asifur@gmail.com>
2016-12-08 18:03:40 +0600

    Updates in driver

--- Drivers/Built_In_RestApi_Driver.py
+++ Drivers/Built_In_RestApi_Driver.py
@@ -40,19 +40,79 @@ def get_method(dependency, run_params, step_data, file_attachment, temp_q):
         return "failed"
 
 
-def post_method(dependency, run_params, step_data, file_attachment, temp_q):
+
+def post_method_single(dependency, run_params, step_data, file_attachment, temp_q):
     sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
     try:
         CommonUtil.ExecLog(sModuleInfo, "Enter: Step - Now We are going to use POST Method", 1, local_run)
 
+        ########### URL ZONE ###########################
 
         url=step_data[0][0][2]
-        param=step_data[1][0][2]
-        statuscode=step_data[2][0][2]
 
-        #sTestStepReturnStatus = Selenium_Built_In.Go_To_Link(web_link)
+        ########### Parameters Zone ####################
+        param_Key = step_data[1][1][0]
+        param_value = step_data[1][1][2]
 
-        sTestStepReturnStatus = REST_Api_Built_In.Data_By_POST_Method(url,param,statuscode)
+        payload={}
+        payload[param_Key]=param_value
+
+        ########## Data_By_POST_Method Usage ######
+
+        sTestStepReturnStatus = REST_Api_Built_In.Data_By_POST_Method(url,payload)
+        temp_q.put(sTestStepReturnStatus)
+
+        CommonUtil.ExecLog(sModuleInfo, "Exit: Step - Data have been Received POST Method", 1, local_run)
+        return sTestStepReturnStatus
+
+    except Exception, e:
+        CommonUtil.ExecLog(sModuleInfo, "Exception :%s" % e, 3, local_run)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        Error_Detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" + "Error Message: " + str(
+            exc_obj) + ";" + "File Name: " + fname + ";" + "Line: " + str(exc_tb.tb_lineno))
+        CommonUtil.ExecLog(sModuleInfo, "Received Wrong Data your link: %s. Error:%s" % (url, Error_Detail), 3,
+                           local_run)
+        CommonUtil.TakeScreenShot(sModuleInfo, local_run)
+        return "failed"
+
+def post_method_multiple(dependency, run_params, step_data, file_attachment, temp_q):
+    sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
+    try:
+        CommonUtil.ExecLog(sModuleInfo, "Enter: Step - Now We are going to use POST Method", 1, local_run)
+
+        ########### URL ZONE ##################
+
+        url=step_data[0][0][2]
+
+        ########### Parameters Zone ###########
+
+        param_Key_1 = step_data[1][1][0]
+        param_Key_2 = step_data[1][2][0]
+        param_Key_3 = step_data[1][3][0]
+
+        param_value_1 = step_data[1][1][2]
+        param_value_2 = step_data[1][2][2]
+        param_value_3 = step_data[1][3][2]
+
+        ############ Payload Zone ##############
+        payload={}
+
+        payload[param_Key_3]=param_value_3
+        payload[param_Key_2]=param_value_2
+        payload[param_Key_1]=param_value_1
+
+        ############ Headers Zone ##############
+
+        header_Key= step_data[2][1][0]
+        header_value = step_data[2][1][2]
+
+        header={}
+        header[header_Key]=header_value
+
+        ########## Data_By_POST_Method_Multiple Usage ######
+
+        sTestStepReturnStatus = REST_Api_Built_In.Data_By_POST_Method_Multiple(url,payload,header)
         temp_q.put(sTestStepReturnStatus)
 
         CommonUtil.ExecLog(sModuleInfo, "Exit: Step - Data have been Received POST Method", 1, local_run)
