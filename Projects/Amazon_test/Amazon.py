6e2e489ae47414a096644d793dd3618dfb9fa1aa
shalder <shalder@assetscience.com>
2017-01-29 10:39:15 +0600

    minor change

--- Projects/Amazon_test/Amazon.py
+++ Projects/Amazon_test/Amazon.py
@@ -14,8 +14,8 @@ from Built_In_Automation.Web.Selenium import BuiltInFunctions
 
 
 # if local_run is True, no logging will be recorded to the web server.  Only local print will be displayed
-local_run = True
-# local_run = False
+#local_run = True
+local_run = False
 
 
 
