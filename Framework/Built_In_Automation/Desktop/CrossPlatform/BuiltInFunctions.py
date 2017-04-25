c2ed382e6cc2b6338463970139d8a4856799daa1
Nafia <nfarchie01@gmail.com>
2017-04-25 09:55:05 +0600

    remove duplicate function

--- Framework/Built_In_Automation/Desktop/CrossPlatform/BuiltInFunctions.py
+++ Framework/Built_In_Automation/Desktop/CrossPlatform/BuiltInFunctions.py
@@ -542,25 +542,3 @@ def Validate_Step_Data(step_data):
 
 
 '===================== ===x=== Validation Section Ends ===x=== ======================'
-
-def remove_a_desktop_folder(step_data):
-    sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
-
-    try:
-        if ((len(step_data[0]) >= 5)):
-            CommonUtil.ExecLog(sModuleInfo,"The information in the data-set(s) are incorrect. Please provide accurate data set(s) information.",
-                                   3)
-            return "failed"
-        else:
-            file_location = FL.get_home_folder() + step_data[0][0][2]
-            result = CommonUtil.run_cmd('rm -rf ' + file_location)
-            CommonUtil.ExecLog(sModuleInfo, "Successfully deleted folder %s", 1)
-            return result
-
-    except Exception, e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        error_detail = ((str(exc_type).replace("type ", "Error Type: ")) + ";" + "Error Message: " + str(exc_obj) +
-                    ";" + "File Name: " + fname + ";" + "Line: " + str(exc_tb.tb_lineno))
-        CommonUtil.ExecLog(sModuleInfo, "Unable to delete folder. Error: %s" % error_detail, 3, local_run)
-        return Failed
