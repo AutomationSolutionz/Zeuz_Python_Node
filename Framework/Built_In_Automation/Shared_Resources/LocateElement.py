0ee103f58aa223f5e369a73fcc72a9df587aad51
Mahabub Chowdhury <nmalamc@yahoo.com>
2017-07-14 11:26:31 -0400

    - fix for http://zeuz.automationsolutionz.com/Home/EditBug/BUG-73/ - fix for http://zeuz.automationsolutionz.com/Home/EditBug/BUG-75/

--- Framework/Built_In_Automation/Shared_Resources/LocateElement.py
+++ Framework/Built_In_Automation/Shared_Resources/LocateElement.py
@@ -187,7 +187,7 @@ def _construct_xpath_list(parameter_list,add_dot=False):
             tag_item = "//"+ filter(lambda x: 'tag' in x, parameter_list)[0][2]
         else:
             tag_item = "//*"
-        if add_dot != False:
+        if add_dot != False and driver_type != 'xml':
             tag_item = '.'+tag_item
         element_main_body_list.append(tag_item)
         #We need to reverse the list so that tag comes at the begining 
@@ -254,8 +254,10 @@ def _get_xpath_or_css_element(element_query,css_xpath, index_number=False):
     try: 
         all_matching_elements = []
         sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
-        if css_xpath == "xpath":
+        if css_xpath == "xpath" and driver_type != 'xml':
             all_matching_elements = WebDriverWait(generic_driver, WebDriver_Wait).until(EC.presence_of_all_elements_located((By.XPATH, element_query)))
+        elif css_xpath == "xpath" and driver_type == 'xml':
+            all_matching_elements = generic_driver.xpath(element_query)
         elif css_xpath == "css":
             all_matching_elements = WebDriverWait(generic_driver, WebDriver_Wait).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, element_query)))
         if len(all_matching_elements)== 0:
