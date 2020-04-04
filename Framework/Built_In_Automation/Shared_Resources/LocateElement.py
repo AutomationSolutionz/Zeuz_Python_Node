19664d4511f1600c668f72b5229a0d604bf8ca9c
riz hossain <hossain.iqbal@gmail.com>
2020-04-04 13:06:59 -0400

    - Waiting warning instead of error.

--- Framework/Built_In_Automation/Shared_Resources/LocateElement.py
+++ Framework/Built_In_Automation/Shared_Resources/LocateElement.py
@@ -133,7 +133,7 @@ def Get_Element(step_data_set,driver,query_debug=False, wait_enable = True):
                     sr.Set_Shared_Variables(save_parameter,result)
                 return result # Return on pass
             if not wait_enable:
-                CommonUtil.ExecLog(sModuleInfo, "Element not found. Waiting is disabled, so returning", 3)
+                CommonUtil.ExecLog(sModuleInfo, "Element not found. Waiting is disabled, so returning", 2)
                 return result # If asked not to loop, return the failure
             # If fail, but instructed to loop, do so
         return "failed"
