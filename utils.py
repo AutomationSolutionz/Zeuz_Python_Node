8f3b360f19daf8771cd0e33076f3e49a3d9793ec
ShaonMahmood <mahmood.habib.cuet@gmail.com>
2020-04-27 23:45:01 +0600

    check connection periodically for poor connection

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import os
+import time
+import select
+import sys
+import platform
 import _thread
 from Framework.Utilities import self_updater
-from tkinter import *
+from tkinter import Toplevel, Label, Button
 
 class MyDialogBox:
 
@@ -30,3 +34,39 @@ class MyDialogBox:
         _thread.start_new_thread(self_updater.main,
                                  (os.path.dirname(os.path.realpath(__file__)).replace(os.sep + 'Framework' + os.sep + 'Utilities', ''),))
         self.root.after(10000, self.root.check_for_updates)  # Checks if install is complete
+
+
+class TimeoutExpired(Exception):
+    pass
+
+def input_with_timeout1(prompt, timeout, timer=time.monotonic):
+    import msvcrt
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    endtime = timer() + timeout
+    result = []
+    while timer() < endtime:
+        if msvcrt.kbhit():
+            result.append(msvcrt.getwche()) #XXX can it block on multibyte characters?
+            if result[-1] == '\n':   #XXX check what Windows returns here
+                return ''.join(result[:-1])
+        time.sleep(0.04) # just to yield to other processes/threads
+    raise TimeoutExpired
+
+
+
+
+def input_with_timeout2(prompt, timeout):
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    ready, _, _ = select.select([sys.stdin], [],[], timeout)
+    if ready:
+        return sys.stdin.readline().rstrip('\n') # expect stdin to be line-buffered
+    raise TimeoutExpired
+
+def input_with_timeout(prompt, timeout):
+    operating_system = platform.system()
+    if operating_system in ["Linux", "Darwin"]:
+        return input_with_timeout2(prompt, timeout)
+    elif operating_system == "Windows":
+        return input_with_timeout1(prompt, timeout)
