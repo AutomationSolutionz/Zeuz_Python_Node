4db9cd863aed55b4c70926e749ee786111c44d6d
sazid <sazidozon@gmail.com>
2020-05-04 03:09:32 +0600

    .

--- Framework/Built_In_Automation/Mobile/CrossPlatform/Appium/BuiltInFunctions.py
+++ Framework/Built_In_Automation/Mobile/CrossPlatform/Appium/BuiltInFunctions.py
@@ -20,6 +20,7 @@ from appium.webdriver.common.touch_action import TouchAction
 from Framework.Built_In_Automation.Shared_Resources import BuiltInFunctionSharedResources as Shared_Resources
 from Framework.Utilities.CommonUtil import passed_tag_list, failed_tag_list, skipped_tag_list
 from Framework.Built_In_Automation.Shared_Resources import LocateElement
+from Framework.Built_In_Automation.Mobile.CrossPlatform.Appium.decorators import filter_optional_action_and_step_data, set_appium_driver
 import psutil
 
 
@@ -145,6 +146,8 @@ def find_exe_in_path(exe):
 # Try to find appium
 appium_binary = ''
 find_appium()
+
+set_appium_driver(appium_driver)
 
 def get_driver():
     ''' For custom functions external to this script that need access to the driver '''
@@ -2419,63 +2422,3 @@ def if_element_exists(data_set):
     except Exception:
         errMsg = "Could not find your element."
         return CommonUtil.Exception_Handler(sys.exc_info(), None, errMsg)
-
-
-# ----------------- Add actions above this line ----------------- #
-
-# -----------------         Decorators          ----------------- #
-def filter_optional_action_and_step_data(action):
-    """
-    Decorator for filtering actions for the following POINTS based on type of row
-    1. ["platform", "optional parameter", "platform name - ios/android"]
-        skips the action if the device platform and selected platform don't match
-    2. ["propertyA | propertyB", "element parameter", "xyz"]
-        returns the following modified data row
-        ["propertyA", "element parameter", "xyz"] if android
-        ["propertyB", "element parameter", "xyz"] if ios
-
-    Tutorial for decorators: https://timber.io/blog/decorators-in-python/
-    """
-
-    import functools
-    @functools.wraps(action)
-
-    def decorated_action(data_set, *args, **kwargs):
-        # This will be passed to the original function
-        cleaned_data_set = []
-
-        device_platform = appium_driver.capabilities['platformName'].strip().lower()
-
-        for row in data_set:
-            left, middle, right = row
-
-            # POINT 1
-            # Skip execution of action if the intended platform does not match with that of the device
-            if "optional parameter" in middle and "platform" in left:
-                if device_platform.strip().lower() == right.strip().lower():
-                    # Skip this row, as its not intended to be a part of the actual step data
-                    new_row = None
-                else:
-                    # Skip executing this action altogether if the intended platform does not match
-                    sModuleInfo = inspect.currentframe().f_code.co_name + " : " + action.__name__
-                    CommonUtil.ExecLog(sModuleInfo,
-                        "[SKIP] This action has been marked as optional and only intended for the platform '%s'" % right.strip(),
-                        1)
-                    return "passed"
-
-            # POINT 2
-            # If we find a '|' character in the left column, then try to check the platform
-            # and filter the appropriate data for the left column by removing '|'
-            if "element parameter" in middle and left.find("|") != -1:
-                if device_platform == "android":
-                    left = left.split("|")[0].strip()
-                elif device_platform == "ios":
-                    left = left.split("|")[1].strip()
-
-            new_row = (left, middle, right,)
-
-            if new_row:
-                cleaned_data_set.append(new_row)
-
-        return action(cleaned_data_set, *args, **kwargs)
-    return decorated_action
