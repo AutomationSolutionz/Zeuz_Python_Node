25979649f120fb3cbf33f11db4978b5995793b84
arif <arifshariar7@gmail.com>
2020-09-29 18:35:59 +0600

    [Add] select_from_db(Action),delete_from_db(Action),insert_into_db(Action),update_into_db(Action)

--- Framework/Built_In_Automation/Sequential_Actions/action_declarations/database.py
+++ Framework/Built_In_Automation/Sequential_Actions/action_declarations/database.py
@@ -3,6 +3,10 @@ declarations = (
     { "name": "connect to db",              "function": "connect_to_db", "screenshot": "none" },
     { "name": "select query",               "function": "db_select",     "screenshot": "none" },
     { "name": "insert update delete query", "function": "db_non_query",  "screenshot": "none" },
+    { "name": "select from db", "function": "select_from_db",  "screenshot": "none" },
+    { "name": "insert into db", "function": "insert_into_db",  "screenshot": "none" },
+    { "name": "delete from db", "function": "delete_from_db",  "screenshot": "none" },
+    { "name": "update into db", "function": "update_into_db",  "screenshot": "none" },
 )
 
 module_name = "database"
