010164ddb1179b329d0e169d6fd4a783f0961915
Muntasib-creator <muntasibchowdhury@gmail.com>
2021-01-28 22:26:10 +0600

    Fixed infinite print of progress bar

--- node_cli.py
+++ node_cli.py
@@ -415,18 +415,19 @@ def RunProcess(sTesterid, user_info_object):
             r = requests.get(RequestFormatter.form_uri("is_submitted_api"), {"machine_name": sTesterid}, verify=False).json()
             Userid = (CommonUtil.MachineInfo().getLocalUser()).lower()
             if r and "found" in r and r["found"]:
-                CommonUtil.ExecLog("", "Downloading dataset and attachments. Please wait", 4)
+                CommonUtil.ExecLog("", "Downloading dataset and attachments of %s KB. Please wait" % (int(r["file_size"])/1000), 4)
                 save_path = temp_ini_file.parent/"attachments"
                 FL.CreateFolder(save_path)
                 response = requests.get(RequestFormatter.form_uri("getting_json_data_api"), {"machine_name": Userid}, stream=True, verify=False)
                 # total_size_in_bytes = int(response.headers.get('content-length', 0))
                 # print(r["file_size"], len(response.content))
                 chunk_size = 4096
-                progress_bar = tqdm(total=r["file_size"], unit='B', mininterval=0, unit_scale=True, miniters=1, leave=True)
+                progress_bar = tqdm(total=r["file_size"], unit='B', mininterval=0, unit_scale=True, unit_divisor=1024, leave=True)
                 with open(save_path/"input.zip", 'wb') as file:
                     for data in response.iter_content(chunk_size):
                         progress_bar.update(len(data))
                         file.write(data)
+                    progress_bar.refresh()
                 z = zipfile.ZipFile(save_path/"input.zip")
                 z.extractall(save_path)
                 z.close()
