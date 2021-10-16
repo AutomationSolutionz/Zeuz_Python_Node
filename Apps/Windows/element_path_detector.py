166056f209c2235b0f4e250b0896c4c3b8d3e303
Muntasib-creator <muntasibchowdhury@gmail.com>
2021-10-16 23:27:52 +0600

    [Improve] Copied the tree

--- Apps/Windows/element_path_detector.py
+++ Apps/Windows/element_path_detector.py
@@ -1,5 +1,9 @@
+import time
+
 import keyboard, autoit, pyautogui
 import os, sys
+import inspect
+from colorama import Fore
 
 new_line = True
 import clr, System
@@ -11,13 +15,45 @@ clr.AddReference(dll_path + "UIAutomationProvider")
 clr.AddReference( "System.Windows.Forms")
 x, y = 0, 0
 from System.Windows.Automation import *
+def ExecLog(sModuleInfo, sDetails, iLogLevel):
+    if iLogLevel == 1:
+        status = "Passed"
+        line_color = Fore.GREEN
+    elif iLogLevel == 2:
+        status = "Warning"
+        line_color = Fore.YELLOW
+    elif iLogLevel == 3:
+        status = "Error"
+        line_color = Fore.RED
+    info = f"{sModuleInfo}\t\n"
+    print(line_color + f"{status.upper()} - {info}{sDetails}")
+
+def Exception_Handler(exec_info):
+    sModuleInfo_Local = inspect.currentframe().f_code.co_name + " : " + inspect.getmodulename(__file__)
+    exc_type, exc_obj, exc_tb = exec_info
+    Error_Type = (
+        (str(exc_type).replace("type ", ""))
+        .replace("<", "")
+        .replace(">", "")
+        .replace(";", ":")
+    )
+    Error_Message = str(exc_obj)
+    File_Name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    Function_Name = os.path.split(exc_tb.tb_frame.f_code.co_name)[1]
+    Line_Number = str(exc_tb.tb_lineno)
+    Error_Detail = (
+        "Error Type ~ %s: Error Message ~ %s: File Name ~ %s: Function Name ~ %s: Line ~ %s"
+        % (Error_Type, Error_Message, File_Name, Function_Name, Line_Number)
+    )
+    sModuleInfo = Function_Name + ":" + File_Name
+    ExecLog(sModuleInfo, "Following exception occurred: %s" % (Error_Detail), 3)
 
 def _found(Element):
     try:
-        left = Element.Current.BoundingRectangle.Left
-        right = Element.Current.BoundingRectangle.Right
-        bottom = Element.Current.BoundingRectangle.Bottom
-        top = Element.Current.BoundingRectangle.Top
+        left = Element.Left
+        right = Element.Right
+        bottom = Element.Bottom
+        top = Element.Top
         if left <= x <= right and top <= y <= bottom:
             return True
         return False
@@ -26,10 +62,10 @@ def _found(Element):
         return False
 
 def create_index(index_trace: dict, element):
-    NameE = element.Current.Name
-    ClassE = element.Current.ClassName
-    AutomationE = element.Current.AutomationId
-    LocalizedControlTypeE = element.Current.LocalizedControlType
+    NameE = element.Name
+    ClassE = element.ClassName
+    AutomationE = element.AutomationId
+    LocalizedControlTypeE = element.LocalizedControlType
 
     s = 'automationid="%s"' % AutomationE
     if s in index_trace: index_trace[s] += 1
@@ -52,10 +88,10 @@ def create_index(index_trace: dict, element):
     else: index_trace[s] = 0
 
 def create_path(index_trace: dict, element):
-    NameE = element.Current.Name
-    ClassE = element.Current.ClassName
-    AutomationE = element.Current.AutomationId
-    LocalizedControlTypeE = element.Current.LocalizedControlType
+    NameE = element.Name
+    ClassE = element.ClassName
+    AutomationE = element.AutomationId
+    LocalizedControlTypeE = element.LocalizedControlType
 
     s_name = 'name="%s"' % NameE
     if NameE and s_name not in index_trace:
@@ -88,8 +124,8 @@ def create_path(index_trace: dict, element):
 def _child_search(ParentElement):
     try:
         path = ""
-        child_elements = ParentElement.FindAll(TreeScope.Children, Condition.TrueCondition)
-        if child_elements.Count == 0:
+        child_elements = ParentElement.children
+        if len(child_elements) == 0:
             return path
 
         index_trace = {}
@@ -105,22 +141,76 @@ def _child_search(ParentElement):
         return path
 
     except Exception:
-        print(sys.exc_info())
+        Exception_Handler(sys.exc_info())
         return ""
 
+class node():
+    def __init__(self, element):
+        self.Name = element.Current.Name
+        self.ClassName = element.Current.ClassName
+        self.AutomationId = element.Current.AutomationId
+        self.LocalizedControlType = element.Current.LocalizedControlType
+        self.AutomationId = element.Current.AutomationId
 
+        try:
+            self.Left = element.Current.BoundingRectangle.Left
+            self.Right = element.Current.BoundingRectangle.Right
+            self.Bottom = element.Current.BoundingRectangle.Bottom
+            self.Top = element.Current.BoundingRectangle.Top
+        except:
+            self.Left = -1
+            self.Right = -1
+            self.Bottom = -1
+            self.Top = -1
+
+        self.parent = None      # Implement it later
+        self.children = []
+
+def copy_tree(Children, ParentElement):
+    try:
+        Children.append(node(ParentElement))
+        Node = Children[-1]
+        child_elements = ParentElement.FindAll(TreeScope.Children, Condition.TrueCondition)
+        if child_elements.Count == 0:
+            return
+        for each_child in child_elements:
+            copy_tree(Node.children, each_child)
+    except:
+        Exception_Handler(sys.exc_info())
 def main():
     try:
         global x, y
-        print("Hover over the desired element and press control")
-        while True:
-            keyboard.wait("ctrl")
-            x, y = pyautogui.position()
-            # print(x, y)
-            res = _child_search(AutomationElement.RootElement)[:-2] + "\n"
-            print(res)
+        print("Press enter to inspect")
+        # time.sleep(5)
+        start = time.time()
+        Root = node(AutomationElement.RootElement)
+        all_windows = AutomationElement.RootElement.FindAll(TreeScope.Children, Condition.TrueCondition)
+        if all_windows.Count != 0:
+            print("Enter between 1-%s to select a window" % all_windows.Count)
+            for i in range(len(all_windows)):
+                print("%s. %s" % (i+1, all_windows[i].Current.Name))
+            dur = time.time() - start
+            idx = input()
+            start = time.time()
+            try:
+                idx = int(idx.strip())
+            except:
+                return Exception_Handler(sys.exc_info())
+            window = all_windows[idx-1]
+            Root.children.append(node(window))
+            all_elements = window.FindAll(TreeScope.Children, Condition.TrueCondition)
+            if all_elements.Count != 0:
+                for each_child in all_elements:
+                    copy_tree(Root.children[0].children, each_child)
+        print("time taken for copy = %s" % (time.time() - start + dur))
+        # import ctypes
+        # ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6)
+        keyboard.wait("ctrl")
+        x, y = pyautogui.position()
+        res = _child_search(Root)[:-2] + "\n"
+        print(res)
     except:
-        print(sys.exc_info())
+        Exception_Handler(sys.exc_info())
 
 
 if __name__ == "__main__":
