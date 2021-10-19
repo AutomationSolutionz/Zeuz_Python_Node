b12c03ceff53f9ae88fde9e927b40823cb9715d4
Muntasib-creator <muntasibchowdhury@gmail.com>
2021-10-19 11:20:26 +0600

    [Add] Dictionary support is added in compare variable action

--- Framework/Built_In_Automation/Shared_Resources/LocateElement.py
+++ Framework/Built_In_Automation/Shared_Resources/LocateElement.py
@@ -231,8 +231,11 @@ def Get_Element(step_data_set, driver, query_debug=False, wait_enable=True, retu
 
         if result not in failed_tag_list:
             if type(result) != list:
-                attribute_parameter = result.get_attribute('outerHTML')
-                CommonUtil.ExecLog(sModuleInfo, "%s" % (attribute_parameter), 5)
+                try:
+                    attribute_parameter = result.get_attribute('outerHTML')
+                    CommonUtil.ExecLog(sModuleInfo, "%s" % (attribute_parameter), 5)
+                except:
+                    pass
             if save_parameter != "":  # save element to a variable
                 sr.Set_Shared_Variables(save_parameter, result)
             return result  # Return on pass
