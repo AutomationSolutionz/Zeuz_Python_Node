8e0ca47431911ca597490071a7b83cc59f3f91c3
Muntasib-creator <muntasibchowdhury@gmail.com>
2021-11-08 22:25:39 +0600

    [Change]dataset=None in teardown_appium

--- Framework/Built_In_Automation/Mobile/CrossPlatform/Appium/BuiltInFunctions.py
+++ Framework/Built_In_Automation/Mobile/CrossPlatform/Appium/BuiltInFunctions.py
@@ -1110,7 +1110,7 @@ def kill_node():
 
 
 @logger
-def teardown_appium(*args):
+def teardown_appium(dataset=None):
     """ Teardown of appium instance """
 
     sModuleInfo = inspect.currentframe().f_code.co_name + " : " + MODULE_NAME
