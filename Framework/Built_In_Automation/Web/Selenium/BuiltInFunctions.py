af40dac72eb3b72c4c954b95d2e2ff2cf58204bd
Muntasib-creator <muntasibchowdhury@gmail.com>
2021-12-31 22:14:17 +0600

    revert

--- Framework/Built_In_Automation/Web/Selenium/BuiltInFunctions.py
+++ Framework/Built_In_Automation/Web/Selenium/BuiltInFunctions.py
@@ -304,9 +304,9 @@ def Open_Electron_App(data_set):
             if "windows" in left and platform.system() == "Windows":
                 desktop_app_path = right.strip()
             elif "mac" in left and platform.system() == "Darwin":
-                desktop_app_path = right
+                desktop_app_path = right.strip()
             elif "linux" in left and platform.system() == "Linux":
-                desktop_app_path = right
+                desktop_app_path = right.strip()
 
         if not desktop_app_path:
             CommonUtil.ExecLog(sModuleInfo, "You did not provide an Electron app path for %s OS" % platform.system(), 3)
@@ -785,7 +785,6 @@ def Go_To_Link(step_data, page_title=False):
 
 @logger
 def Handle_Browser_Alert(step_data):
-    global selenium_driver
     # accepts browser alert
     """
     wait           optional parameter  5.0
@@ -821,18 +820,18 @@ def Handle_Browser_Alert(step_data):
 
     try:
         if choice_lower in ("accept", "pass", "yes", "ok"):
-            selenium_driver.switch_to.alert.accept()
+            selenium_driver.switch_to_alert().accept()
             CommonUtil.ExecLog(sModuleInfo, "Browser alert accepted", 1)
             return "passed"
 
         elif choice_lower in ("reject", "fail", "no", "cancel"):
-            selenium_driver.switch_to.alert.dismiss()
+            selenium_driver.switch_to_alert().dismiss()
             CommonUtil.ExecLog(sModuleInfo, "Browser alert rejected", 1)
             return "passed"
 
         elif "get text" in choice_lower:
-            alert_text = selenium_driver.switch_to.alert.text
-            selenium_driver.switch_to.alert.accept()
+            alert_text = selenium_driver.switch_to_alert().text
+            selenium_driver.switch_to_alert().accept()
             variable_name = (choice.split("="))[1]
             result = Shared_Resources.Set_Shared_Variables(
                 variable_name, alert_text
@@ -849,8 +848,8 @@ def Handle_Browser_Alert(step_data):
 
         elif "send text" in choice_lower:
             text_to_send = (choice.split("="))[1]
-            selenium_driver.switch_to.alert.send_keys(text_to_send)
-            selenium_driver.switch_to.alert.accept()
+            selenium_driver.switch_to_alert().send_keys(text_to_send)
+            selenium_driver.switch_to_alert().accept()
             return "passed"
 
         else:
@@ -3555,7 +3554,7 @@ def upload_file(step_data):
 
 # Method to upload file
 @logger
-def drag_and_drop(step_data):
+def drag_and_drop(dataset):
     sModuleInfo = inspect.currentframe().f_code.co_name + " : " + MODULE_NAME
     global selenium_driver
     try:
@@ -3577,25 +3576,24 @@ def drag_and_drop(step_data):
                 source.append((left, mid, right))
                 destination.append((left, mid, right))
 
-        if source == "":
-            CommonUtil.ExecLog(
-                sModuleInfo, "No source element specified for drag and drop", 3
-            )
+        if not source:
+            CommonUtil.ExecLog(sModuleInfo, 'Please provide source element with "src element parameter", "src parent parameter" etc. Example:\n'+
+               "(id, src element parameter, file)", 3)
             return "zeuz_failed"
-        elif destination == "":
-            CommonUtil.ExecLog(
-                sModuleInfo, "No destination element specified for drag and drop", 3
-            )
+        if not destination:
+            CommonUtil.ExecLog(sModuleInfo, 'Please provide Destination element with "dst element parameter", "dst parent parameter" etc. Example:\n'+
+               "(id, dst element parameter, table)", 3)
             return "zeuz_failed"
 
-        source_element = Shared_Resources.Get_Shared_Variables(source)
-        if source_element in failed_tag_list:
-            CommonUtil.ExecLog(
-                sModuleInfo,
-                "No element found in shared variables named '%s' which is defined as source for drag and drop",
-                source,
-                3,
-            )
+        source_element = LocateElement.Get_Element(source, selenium_driver)
+        if source_element == "zeuz_failed":
+            CommonUtil.ExecLog(sModuleInfo, "Source Element is not found", 3)
+            return "zeuz_failed"
+
+        destination_element = LocateElement.Get_Element(destination, selenium_driver)
+        if destination_element == "zeuz_failed":
+            CommonUtil.ExecLog(sModuleInfo, "Destination Element is not found", 3)
+            return "zeuz_failed"
 
         # ActionChains(selenium_driver).drag_and_drop(source_element, destination_element).perform()
         ActionChains(selenium_driver).click_and_hold(source_element).move_to_element(destination_element).pause(0.5).release(destination_element).perform()
--- Framework/Utilities/CommonUtil.py
+++ Framework/Utilities/CommonUtil.py
@@ -677,7 +677,7 @@ def set_screenshot_vars(shared_variables):
 
 def TakeScreenShot(function_name, local_run=False):
     """ Puts TakeScreenShot into a thread, so it doesn't block test case execution """
-    # if debug_status: return
+    # if debug_status: return     # Todo: Comment this line out
     try:
         if upload_on_fail and rerun_on_fail and not rerunning_on_fail and not debug_status:
             return
@@ -1044,8 +1044,22 @@ def debug_code_error(exc_info):
     print(Error_Detail)
 
 
-
 def path_parser(path: str) -> str:
+    r"""
+    Case-1: (Full_path)
+    C:\Users\ASUS\entreprize_5689.csv
+    Case-2: (Home_dir)
+    ~\Downloads\entreprize_5689.csv
+    Case-3: (Partial_search)
+    ~\Downloads\*entreprize_.csv
+    ~\Downloads\*entreprize_
+    Case-4: (Multiple_Partial_Search)
+    ~\Downloads\*server\*entreprize_.csv
+    Case-5: (Partial_Case-insensitive_Search)
+    ~\Downloads\**server\**entreprize_.csv
+    Case-6: (Partial search with Index)
+    ~\Downloads\**server\[idx]*entreprize_.csv
+    """
     if path.startswith("~"):
         path1 = str(Path("~\\" + path.split("\\")[1]).expanduser())
         for i in path.split("\\")[2:]:
