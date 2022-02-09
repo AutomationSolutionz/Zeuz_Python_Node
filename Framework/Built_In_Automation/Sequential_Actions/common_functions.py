52fccaf7819780bfc382855c095e6b2d910023b6
Arifshariar7 <arifshariar7@gmail.com>
2022-02-09 21:39:00 +0600

    changing diff of html file formate

--- Framework/Built_In_Automation/Sequential_Actions/common_functions.py
+++ Framework/Built_In_Automation/Sequential_Actions/common_functions.py
@@ -4511,7 +4511,7 @@ def compare_file(data_set):
                 date = datetime.datetime.now(). strftime("%Y_%m_%d_%I_%M_%S_%p")
                 f1 = open(parentpath).read()
                 f2 = open(childpath).read()
-                diff_html = diff(f1, f2)
+                diff_html = difflib.HtmlDiff().make_file(f1,f2)
                 test_case = ConfigModule.get_config_value("sectionOne", "test_case", temp_config)
                 test_case_folder = ConfigModule.get_config_value("sectionOne", "test_case_folder", temp_config)
                 with open(test_case_folder+os.sep+f"{test_case}_{date}.html", "w") as f:
@@ -4552,9 +4552,9 @@ def compare_file(data_set):
                 date = datetime.datetime.now().strftime("%Y_%m_%d_%I_%M_%S_%p")
                 test_case_folder = ConfigModule.get_config_value("sectionOne", "test_case_folder", temp_config)
                 test_case = ConfigModule.get_config_value("sectionOne", "test_case", temp_config)
-                f1 = open(parentpath).read()
-                f2 = open(childpath).read()
-                diff_html = diff(f1, f2)
+                f1 = open(parentpath).readlines()
+                f2 = open(childpath).readlines()
+                diff_html = difflib.HtmlDiff().make_file(f1,f2)
                 with open(test_case_folder+os.sep+f"{test_case}_{date}.html", "w") as f:
                     f.write(diff_html)
 
