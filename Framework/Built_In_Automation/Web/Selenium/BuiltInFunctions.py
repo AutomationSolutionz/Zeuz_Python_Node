e668b3dc125152147f7479047740468a65a6acf8
Muntasib-creator <muntasibchowdhury@gmail.com>
2022-03-30 13:12:07 +0600

    [Add] Re-added android browser chrome and Alert() function and pyautogui fix

--- Framework/Built_In_Automation/Web/Selenium/BuiltInFunctions.py
+++ Framework/Built_In_Automation/Web/Selenium/BuiltInFunctions.py
@@ -18,7 +18,6 @@ import sys, os, time, inspect, shutil, subprocess
 import socket
 import requests
 import psutil
-import pyautogui
 
 sys.path.append("..")
 from selenium import webdriver
@@ -37,6 +36,7 @@ from selenium.common.exceptions import ElementClickInterceptedException, WebDriv
     SessionNotCreatedException, TimeoutException, NoSuchFrameException, StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support import expected_conditions as EC
 import selenium
 
@@ -389,7 +389,7 @@ def Open_Browser(dependency, window_size_X=None, window_size_Y=None):
         CommonUtil.teardown = True
         browser = browser.lower().strip()
 
-        if browser in ("android", "ios"):
+        if browser in ("ios",):
             # Finds the appium binary and starts the server.
             appium_port = start_appium_server()
 
@@ -422,7 +422,7 @@ def Open_Browser(dependency, window_size_X=None, window_size_Y=None):
             selenium_driver = appiumdriver.Remote("http://localhost:%d/wd/hub" % appium_port, capabilities)
             selenium_driver.implicitly_wait(WebDriver_Wait)
 
-        elif browser in ("chrome", "chromeheadless"):
+        elif browser in ("android", "chrome", "chromeheadless"):
             from selenium.webdriver.chrome.options import Options
             chrome_path = ConfigModule.get_config_value("Selenium_driver_paths", "chrome_path")
             if not chrome_path:
@@ -880,42 +880,35 @@ def Handle_Browser_Alert(step_data):
 
     try:
         if choice_lower in ("accept", "pass", "yes", "ok"):
-            selenium_driver.switch_to_alert().accept()
+            Alert(selenium_driver).accept()
             CommonUtil.ExecLog(sModuleInfo, "Browser alert accepted", 1)
             return "passed"
 
-        elif choice_lower in ("reject", "fail", "no", "cancel"):
-            selenium_driver.switch_to_alert().dismiss()
+        elif choice_lower in ("reject", "decline", "dismiss", "fail", "no", "cancel"):
+            Alert(selenium_driver).dismiss()
             CommonUtil.ExecLog(sModuleInfo, "Browser alert rejected", 1)
             return "passed"
 
-        elif "get text" in choice_lower:
-            alert_text = selenium_driver.switch_to_alert().text
-            selenium_driver.switch_to_alert().accept()
-            variable_name = (choice.split("="))[1]
-            result = Shared_Resources.Set_Shared_Variables(
-                variable_name, alert_text
-            )
-            if result in failed_tag_list:
-                CommonUtil.ExecLog(
-                    sModuleInfo,
-                    "Value of Variable '%s' could not be saved!!!" % variable_name,
-                    3,
-                )
-                return "zeuz_failed"
-            else:
-                return "passed"
+        elif choice_lower.replace(" ", "").replace("_", "").startswith("gettext"):
+            alert_text = Alert(selenium_driver).text
+            Alert(selenium_driver).accept()
+            variable_name = (choice.split("="))[1].strip()
+            return Shared_Resources.Set_Shared_Variables(variable_name, alert_text)
 
-        elif "send text" in choice_lower:
-            text_to_send = (choice.split("="))[1]
-            selenium_driver.switch_to_alert().send_keys(text_to_send)
-            selenium_driver.switch_to_alert().accept()
+        elif choice_lower.replace(" ", "").replace("_", "").startswith("sendtext"):
+            text_to_send = (choice.split("="))[1].strip()
+            Alert(selenium_driver).send_keys(text_to_send)
+            Alert(selenium_driver).accept()
             return "passed"
 
         else:
             CommonUtil.ExecLog(
                 sModuleInfo,
-                "Wrong Step Data.  Please review the action help document",
+                "Wrong Step Data. The following are valid data --\n" +
+                "1. (handle alert, selenium action, ok)" +
+                "2. (handle alert, selenium action, cancel)" +
+                "3. (handle alert, selenium action, get text = var_name)" +
+                "4. (handle alert, selenium action, send text = some text)",
                 3,
             )
             return "zeuz_failed"
@@ -1343,6 +1336,7 @@ def Click_and_Download(data_set):
     """ Click and download attachments from web and save it to specific destinations"""
     sModuleInfo = inspect.currentframe().f_code.co_name + " : " + MODULE_NAME
     global selenium_driver
+    import pyautogui
 
     if selenium_driver.capabilities["browserName"].strip().lower() not in ("chrome", "msedge", "firefox"):
         CommonUtil.ExecLog(sModuleInfo, "This action was made for Chrome, MS Edge and Firefox. Other browsers won't download files in Zeuz_Download_Folder", 2)
@@ -3737,6 +3731,7 @@ def upload_file(step_data):
 
 def _gui_upload(path_name, pid=None):
     # Todo: Implement PID to activate the window and focus that at front
+    import pyautogui
     time.sleep(3)
     pyautogui.hotkey("alt", "a")
     time.sleep(0.5)
@@ -3766,6 +3761,7 @@ def upload_file_through_window(step_data):
     global selenium_driver
     all_file_path = []
     pid = ""
+    import pyautogui
     if "headless" in dependency:
         CommonUtil.ExecLog(sModuleInfo, "This action will not work on headless browsers", 3)
         return "zeuz_failed"
