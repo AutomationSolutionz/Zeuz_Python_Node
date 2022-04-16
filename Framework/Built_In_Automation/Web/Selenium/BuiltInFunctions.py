ae5d1b5c86432731265334a6117f30c9ebaccc64
Muntasib-creator <muntasibchowdhury@gmail.com>
2022-04-16 16:48:03 +0600

    [Change] Changed the import pyautogui position for Click_and_Download action

--- Framework/Built_In_Automation/Web/Selenium/BuiltInFunctions.py
+++ Framework/Built_In_Automation/Web/Selenium/BuiltInFunctions.py
@@ -1358,7 +1358,6 @@ def Click_and_Download(data_set):
     """ Click and download attachments from web and save it to specific destinations"""
     sModuleInfo = inspect.currentframe().f_code.co_name + " : " + MODULE_NAME
     global selenium_driver
-    import pyautogui
 
     if selenium_driver.capabilities["browserName"].strip().lower() not in ("chrome", "msedge", "firefox"):
         CommonUtil.ExecLog(sModuleInfo, "This action was made for Chrome, MS Edge and Firefox. Other browsers won't download files in Zeuz_Download_Folder", 2)
@@ -1414,6 +1413,7 @@ def Click_and_Download(data_set):
                         ]
                         if win_Click_Element(save_click_ds) == "zeuz_failed":
                             CommonUtil.ExecLog(sModuleInfo, "Could not click Save Button. Switching to GUI method", 2)
+                            import pyautogui
                             pyautogui.hotkey("down")
                             pyautogui.hotkey("enter")
 
@@ -1435,6 +1435,7 @@ def Click_and_Download(data_set):
                             ]
                             if win_Click_Element(ok_ds) == "zeuz_failed":
                                 CommonUtil.ExecLog(sModuleInfo, "Could not find the OK button. Switching to GUI method (pressing Enter)", 2)
+                                import pyautogui
                                 pyautogui.hotkey("enter")
                 except:
                     CommonUtil.ExecLog(sModuleInfo, "Could not check if any save window was opened. Continuing...", 2)
