b9b8398366686d7480c93b1f9e311bbc538511b6
oytizzo <oytizzo@gmail.com>
2022-07-01 18:52:42 +0600

    Update settings.py for report dirs

--- settings.py
+++ settings.py
@@ -4,11 +4,17 @@ from pathlib import Path
 from Framework.Utilities import ConfigModule
 
 
-# BASE_DIR or PROJECT_ROOT
+# BASE_DIR or PROJECT_ROOT or Zeuz_Python_Node dir
 PROJECT_ROOT = Path(__file__).parent
 
-# AutomationLog directory
+# AutomationLog dir
 AutomationLog_DIR = PROJECT_ROOT / "AutomationLog"
+
+# attachments dir
+attachments_DIR = AutomationLog_DIR / "attachments"
+
+# settings.conf file
+settings_conf_dir = PROJECT_ROOT / "Framework" / "settings.conf"
 
 # temp_ini_file = os.path.join(
 #         os.path.join(
@@ -20,6 +26,9 @@ AutomationLog_DIR = PROJECT_ROOT / "AutomationLog"
 #         )
 #     )
 
+# temp_config.ini file
+# temp_ini_file = AutomationLog_DIR / "temp_config.ini"
+# temp_ini_file = AutomationLog_DIR / ConfigModule.get_config_value("Advanced Options", "_file", settings_conf_dir)
 temp_ini_file = AutomationLog_DIR / ConfigModule.get_config_value("Advanced Options", "_file")
 
 performance_report_dir = ConfigModule.get_config_value("sectionOne", "performance_report", temp_ini_file)
