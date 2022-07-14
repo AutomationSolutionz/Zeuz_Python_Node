2ed93b1d89e3f45ec84af24a61cdc77c79ae8996
Sakib75 <skb09skb@gmail.com>
2022-07-14 14:03:15 +0600

    datetime format for windows

--- Framework/Built_In_Automation/Shared_Resources/BuiltInFunctionSharedResources.py
+++ Framework/Built_In_Automation/Shared_Resources/BuiltInFunctionSharedResources.py
@@ -1363,12 +1363,14 @@ def save_built_in_time_variable(string):
 
         input = str(string).lower().strip()
 
+        if "today" in input:
+            if os.name == "nt":
+                datetime_format = datetime_format.replace("%-d", "%#d").replace("%-m", "%#m").replace("%-H", "%#H").replace("%-I", "%#I").replace("%-M", "%#M").replace("%-S", "%#S").replace("%-j", "%#j")
+
         if input == "currentepochtime":
             return int(time.time())
         elif input.startswith("today"):
             if input.lower().strip() == "today":
-                if os.name == "nt":
-                    datetime_format = datetime_format.replace("%-d", "%#d").replace("%-m", "%#m").replace("%-H", "%#H").replace("%-I", "%#I").replace("%-M", "%#M").replace("%-S", "%#S").replace("%-j", "%#j")
                 return datetime.today().strftime(datetime_format)
             elif "+" in input:
                 l = input.split("+")
