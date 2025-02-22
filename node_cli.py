de32d1e3d408f14b8e06b537e387d8de977bed88
sazid <sazidozon@gmail.com>
2025-02-22 23:54:50 +0600

    Change default node server port

--- node_cli.py
+++ node_cli.py
@@ -48,7 +48,7 @@ from server import main as node_server
 def start_server():
     def run():
         # TODO: the port must be selected dynamically from a range of ports - max 50 ports maybe?
-        uvicorn.run(node_server.main(), host="127.0.0.1", port=18000, log_level="warning")
+        uvicorn.run(node_server.main(), host="127.0.0.1", port=18100, log_level="warning")
 
     t = threading.Thread(target=run, daemon=True)
     t.start()
