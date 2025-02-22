58920d1de8d1880fd0be9c379a95d1ccea3d2892
sazid <sazidozon@gmail.com>
2025-02-22 22:12:14 +0600

    filter out the status endpoint from the logs to avoid spamming

--- server/main.py
+++ server/main.py
@@ -1,3 +1,5 @@
+import logging
+from typing import Any
 from fastapi import APIRouter, FastAPI
 
 from server.status import router as status_router
@@ -6,7 +8,23 @@ from server.evaluator import router as evaluator_router
 from server.node_operator import router as operator_router
 
 
+class EndpointFilter(logging.Filter):
+    def __init__(
+        self,
+        path: str,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        super().__init__(*args, **kwargs)
+        self._path = path
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.getMessage().find(self._path) == -1
+
 def main() -> FastAPI:
+    # Filter out /endpoint
+    logging.getLogger("uvicorn.access").addFilter(EndpointFilter(path="/status"))
+
     v1router = APIRouter(
         prefix="/api/v1",
         tags=["v1"],
