4409f3508901ead3088e39fe3b3311742ac3f656
Mohammed Sazid Al Rashid <sazidozon@gmail.com>
2025-05-19 16:59:06 +0600

    Fix the data shape when reading data from datastore

--- Framework/Built_In_Automation/Sequential_Actions/common_functions.py
+++ Framework/Built_In_Automation/Sequential_Actions/common_functions.py
@@ -5944,9 +5944,7 @@ def data_store_read(data_set):
         )
         if res.status_code == 200:
             response_json = json.loads(res.text)
-            response_json = {
-                "data": response_json,
-            }
+            response_json = response_json["data"]
             return sr.Set_Shared_Variables(var_name, response_json, pretty=True)
         else:
             CommonUtil.ExecLog(sModuleInfo, "No data found, please check your dataset", 1)
