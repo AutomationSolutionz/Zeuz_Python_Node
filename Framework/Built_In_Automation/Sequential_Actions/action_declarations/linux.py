8290f6a56fdbec85ca594754f57d0890ab50390a
Mahmudul Alam <mahmudula2000@gmail.com>
2025-07-30 08:36:19 +0600

    added wait to appear, wait to disappear and save attributes

--- Framework/Built_In_Automation/Sequential_Actions/action_declarations/linux.py
+++ Framework/Built_In_Automation/Sequential_Actions/action_declarations/linux.py
@@ -2,6 +2,9 @@ declarations = (
     { "name": "open app",        "function": "open_app",                "screenshot": "desktop" },
     { "name": "click",           "function": "click_element",           "screenshot": "desktop" },
     { "name": "text",            "function": "enter_text",              "screenshot": "desktop" },
+    { "name": "wait to appear",  "function": "wait_for_element",        "screenshot": "desktop" },
+    { "name": "wait to disappear","function": "wait_for_element",       "screenshot": "desktop" },
+    { "name": "save attribute",  "function": "save_attribute",          "screenshot": "desktop" },
 )
 
 module_name = "linux"
