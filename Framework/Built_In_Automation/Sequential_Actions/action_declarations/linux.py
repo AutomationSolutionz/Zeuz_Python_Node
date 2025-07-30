5ae5b458cc47901f27f3909337194f6a349d7420
Mahmudul Alam <mahmudula2000@gmail.com>
2025-07-30 09:04:02 +0600

    added send keystroke linux action

--- Framework/Built_In_Automation/Sequential_Actions/action_declarations/linux.py
+++ Framework/Built_In_Automation/Sequential_Actions/action_declarations/linux.py
@@ -5,6 +5,7 @@ declarations = (
     { "name": "wait to appear",  "function": "wait_for_element",        "screenshot": "desktop" },
     { "name": "wait to disappear","function": "wait_for_element",       "screenshot": "desktop" },
     { "name": "save attribute",  "function": "save_attribute",          "screenshot": "desktop" },
+    { "name": "keystroke keys",  "function": "send_keystroke",  "screenshot": "desktop" },
 )
 
 module_name = "linux"
