b9ea125b704a6818dddcb96bb50cdb1ed27f405c
nasif-z <93877382+nasif-z@users.noreply.github.com>
2025-09-23 23:25:04 +0600

    Enable Windows notification for both debug and deployment test case runs

--- node_cli.py
+++ node_cli.py
@@ -529,8 +529,7 @@ def RunProcess(node_id, run_once=False, log_dir=None):
                 return False
 
             print("[deploy] Run complete.")
-            if CommonUtil.debug_status:
-                notify_complete("Run completed")
+            notify_complete("Run completed")
 
             if run_once:
                 return True
@@ -542,8 +541,7 @@ def RunProcess(node_id, run_once=False, log_dir=None):
                 return
 
             print("[deploy] Run cancelled.")
-            if CommonUtil.debug_status:
-                notify_complete("Run cancelled")
+            notify_complete("Run cancelled")
             CommonUtil.run_cancelled = True
 
         deploy_handler = long_poll_handler.DeployHandler(
