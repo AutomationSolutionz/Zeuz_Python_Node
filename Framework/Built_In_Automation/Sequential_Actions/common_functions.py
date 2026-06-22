208962d4d7e2ac2208da2ab863b35877808f91f9
mdshakib007 <shakibahmed.528874@gmail.com>
2026-06-22 10:00:00 +0600

    removed changes

--- Framework/Built_In_Automation/Sequential_Actions/common_functions.py
+++ Framework/Built_In_Automation/Sequential_Actions/common_functions.py
@@ -619,11 +619,7 @@ def Sleep(data_set):
     try:
         seconds = float(data_set[0][2])
         CommonUtil.ExecLog(sModuleInfo, f"Sleeping for {seconds} seconds", 1)
-        elapsed = 0.0
-        chunk = 0.1
-        while elapsed < seconds:
-            time.sleep(min(chunk, seconds - elapsed))
-            elapsed += chunk
+        time.sleep(seconds)
         return "passed"
     except Exception:
         return CommonUtil.Exception_Handler(sys.exc_info())
